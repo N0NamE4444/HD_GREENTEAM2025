85c847f189f8c1b82a0320491b5976ede2496531
Hrbolek <alexandr.stefek@unob.cz>
2024-09-10 10:08:29 +0200

    support for cockroachDB

--- server/users.py
+++ server/users.py
@@ -31,6 +31,7 @@ def ComposeConnectionString():
 
     driver = "postgresql+asyncpg"  # "postgresql+psycopg2"
     connectionstring = f"{driver}://{user}:{password}@{hostWithPort}/{database}"
+    connectionstring = os.environ.get("CONNECTION_STRING", connectionstring)
 
     return connectionstring
 
